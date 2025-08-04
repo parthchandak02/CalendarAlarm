df3be8d21233fd9390f45aaef1381294fb8a74d0
Parth Chandak <parth.chandak02@gmail.com>
2025-08-04 14:39:28 -0700

    Initial Commit

---
+++ CalendarAlarmApp/CalendarAlarmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAlarmAppApp.swift
+//  CalendarAlarmApp
+//
+//  Created by Parth Chandak on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAlarmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAlarmApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalendarAlarmApp
+//
+//  Created by Parth Chandak on 8/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
